db688e1639a37533cb3aa6b5d9aeacb8589d3a94
Trie0037 <trie0037@gmail.com>
2020-01-18 23:44:03 -0600

    sudo

--- script.js
+++ script.js
@@ -13,28 +13,32 @@ const WINNING_COMBOS = [
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.getElementById("board");
 const winningMessageElement = document.getElementById("winningMessage");
-const newGameBtn = document.getElementById('newGameBtn')
+const newGameBtn = document.getElementById("newGameBtn");
 const winningMessageTextElement = document.querySelector(
   "[data-winning-message-text]"
 );
 let circleTurn;
 
+// Start Game
 beginGame();
 
-newGameBtn.addEventListener('click', beginGame)
+// Starting or restarting click listener
+newGameBtn.addEventListener("click", beginGame);
 
+// Clears previous game and sets fresh grid
 function beginGame() {
   circleTurn = false;
   cellElements.forEach(cell => {
-      cell.classList.remove(X_CLASS)
-      cell.classList.remove(CIRCLE_CLASS)
-      cell.removeEventListener('click', handleClick)
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(CIRCLE_CLASS);
+    cell.removeEventListener("click", handleClick);
     cell.addEventListener("click", handleClick, { once: true });
   });
   setBoardHoverClass();
-  winningMessageElement.classList.remove('show')
+  winningMessageElement.classList.remove("show");
 }
 
+// Determines whether game was won, or a draw, then sets turn accordingly
 function handleClick(e) {
   const cell = e.target;
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
@@ -49,29 +53,36 @@ function handleClick(e) {
   }
 }
 
+// X Win, O Win, or Tie display message
 function gameOver(tie) {
   if (tie) {
-      winningMessageTextElement.innerText = 'Tie!'
+    winningMessageTextElement.innerText = "Tie!";
   } else {
     winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Won!`;
   }
   winningMessageElement.classList.add("show");
 }
 
+// Recognizes grid cells are all filled-in
 function isTie() {
-    return [...cellElements].every(cell => {
-        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-    })
+  return [...cellElements].every(cell => {
+    return (
+      cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    );
+  });
 }
 
+// X's or O's
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
 }
 
+// New player turn based on results
 function switchTurn() {
   circleTurn = !circleTurn;
 }
 
+// Hover effects based on player X or Circle's turn
 function setBoardHoverClass() {
   board.classList.remove(X_CLASS);
   board.classList.remove(CIRCLE_CLASS);
@@ -82,6 +93,7 @@ function setBoardHoverClass() {
   }
 }
 
+// Deciphers winning combinations
 function winCheck(currentClass) {
   return WINNING_COMBOS.some(combos => {
     return combos.every(index => {
